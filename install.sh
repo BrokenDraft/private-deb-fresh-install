dae48f56c1bb5d52d814b0dfc8d343001cf0d5e7
BrokenDraft <robinsoudier@protonmail.com>
2022-03-18 10:33:52 +0100

    aliases

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/BrokenDraft/Oh-my-zsh_deb_Full-install/main/good_boi.sh)"
 git clone git@github.com:BrokenDraft/.linux-aliases.git
 cp -r .linux-aliases ~/ ; rm .linux-aliases
+cd ~/ ; cp .zshrc .zshrc-old
+curl https://raw.githubusercontent.com/BrokenDraft/private-deb-fresh-install/main/alias-sync.txt?token=GHSAT0AAAAAABSSG337SQDFRDIKQ3DEMQBSYRUKD6Q >> .zshrc
+source .zshrc
 sudo apt install python3
 sudo apt install tmux
-git clone https://github.com/BrokenDraft/nanorc-file.git
+repo ; git clone https://github.com/BrokenDraft/nanorc-file.git
 cd nanorc-file ; sudo ./add_nanorc.sh
